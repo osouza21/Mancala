7ea069b5a88bbd22c9661d1ff5e5b3a18ab34c90
RyanEager <Ryan@Eager.com>
2012-12-01 22:45:35 -0800

    Added player names to board

--- src/mancala/MancalaView.java
+++ src/mancala/MancalaView.java
@@ -3,11 +3,13 @@ package mancala;
 //test Ryan
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -30,15 +32,34 @@ public class MancalaView implements ChangeListener{
         pits = new JButton[2][LENGTH];
         board = gameModel.getBoard();
         
+        //settin up frame
         frame = new JFrame();
         frame.setLayout(new BorderLayout());
         boardsetup();
         
+        //mancal's
         frame.add(pits[0][6], BorderLayout.WEST);
         frame.add(pits[1][6], BorderLayout.EAST);
         
+        //lables for pits
+        JLabel PlayerA = new JLabel("Player 0");
+        PlayerA.setFont(new Font("Serif", Font.PLAIN, 26));
+        JLabel PlayerB = new JLabel("Player 1");
+        PlayerB.setFont(new Font("Serif", Font.PLAIN, 26));
+        //frame.add(PlayerA, BorderLayout.NORTH);
+        
+        //Player A's pits
         JPanel topPanel = new JPanel();
-        topPanel.setLayout(new GridLayout(0,6));        
+        topPanel.setLayout(new GridLayout(2,8)); 
+        topPanel.add(new JLabel(""));
+        topPanel.add(new JLabel(""));
+        topPanel.add(new JLabel(""));
+        topPanel.add(PlayerA);
+        topPanel.add(new JLabel(""));
+        topPanel.add(new JLabel(""));
+        topPanel.add(new JLabel(""));
+        topPanel.add(new JLabel(""));
+        topPanel.add(new JLabel(""));
         topPanel.add(pits[0][5]);
         topPanel.add(pits[0][4]);
         topPanel.add(pits[0][3]);
@@ -46,26 +67,36 @@ public class MancalaView implements ChangeListener{
         topPanel.add(pits[0][1]);
         topPanel.add(pits[0][0]);
         frame.add(topPanel, BorderLayout.NORTH);
-
+        
+        //Player B's pits
         JPanel bottomPanel = new JPanel();
-        bottomPanel.setLayout(new GridLayout(0,6));
+        bottomPanel.setLayout(new GridLayout(2,8));
+        bottomPanel.add(new JLabel(""));
         bottomPanel.add(pits[1][0]);
         bottomPanel.add(pits[1][1]);
         bottomPanel.add(pits[1][2]);
         bottomPanel.add(pits[1][3]);
         bottomPanel.add(pits[1][4]);
         bottomPanel.add(pits[1][5]);
+        bottomPanel.add(new JLabel(""));
+        bottomPanel.add(new JLabel(""));
+        bottomPanel.add(new JLabel(""));
+        bottomPanel.add(new JLabel(""));
+        bottomPanel.add(PlayerB);
+        bottomPanel.add(new JLabel(""));
+        bottomPanel.add(new JLabel(""));
+        bottomPanel.add(new JLabel(""));
         frame.add(bottomPanel, BorderLayout.SOUTH);
        
+        //setup undo button
         undo = new JButton("Undo player " + gameModel.getActive());
         undo.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
                 gameModel.undo();
-                gameModel.notifyListeners();
-                
-                
+                gameModel.notifyListeners(); 
             }
         });
+        
         frame.add(undo,BorderLayout.CENTER);
         updatePits();
         
