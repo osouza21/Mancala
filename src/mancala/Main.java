dd53388eb3c42be3143457cc21b06265a1a9c0d8
Alex Souza <osouza21@gmail.com>
2012-12-01 21:44:58 -0800

    fixed main to actually run

--- src/mancala/Main.java
+++ src/mancala/Main.java
@@ -8,7 +8,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MancalaView view = new MancalaView(3);
     }
 
 }
