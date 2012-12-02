325c6d5cdd485882a3d0ae39247305485afd16a8
Alex Souza <osouza21@gmail.com>
2012-12-02 00:50:18 -0800

    minor changes still working on undo

--- src/mancala/Model.java
+++ src/mancala/Model.java
@@ -15,13 +15,17 @@ public class Model {
     private int[][] board;
     private int[][] boardSave;
     private int active;
-    private int undoA;
-    private int undoB;
     private boolean gameover;
+    private int undos;
+    private boolean done;
+    private boolean canGo;
     ArrayList<ChangeListener> listeners = new ArrayList<ChangeListener>();
+    
+    
 
     public Model(int startingPits) {
         board = new int[2][LENGTH];
+        boardSave = new int[2][LENGTH];
         
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < LENGTH; j++) {
@@ -33,9 +37,11 @@ public class Model {
             }
         }
         active = 0;
-        undoA = 3;
-        undoB = 3;
+        undos = 3;
         gameover = false;
+        canGo = true;
+        done = false;
+                
     }
 
     /**
@@ -61,18 +67,20 @@ public class Model {
     * @param side 
     */
     public void turn(int pit, int side) {
-        this.saveBoard();
-        System.out.println("side: " + side + " pit: " + pit);
-        undoA = 3;
-        undoB = 3;
-        if (side != active) {
+        
+        if(side != active)
+        {
             return;
         }
+        if(board[side][pit] == 0){
+            return;
+        }
+        this.saveBoard();
         int held = board[side][pit];
         board[side][pit] = 0;
         int index = pit;
         int sideIndex = side;
-        while (held > 0 && !gameover) {
+        while (held > 0 && canGo) {
             if (index >= LENGTH - 1)// reached end of board
             {
                 index = 0;
@@ -90,6 +98,8 @@ public class Model {
             board[sideIndex][index]++;
             held--;
         }
+        canGo = false;
+        //steal from opposite players pit
         if (sideIndex == active && index < LENGTH - 1 && board[sideIndex][index] == 1) {
             
             if(board[switchPlayer(sideIndex)][5 - index] != 0)
@@ -98,10 +108,13 @@ public class Model {
                 board[switchPlayer(sideIndex)][5 - index] = 0;
                 board[sideIndex][index] = 0;
             }
-            
-
         }
-        if (sideIndex != active || index != LENGTH - 1) {
+        
+        if (sideIndex == active && index == LENGTH - 1) {
+            canGo = true;
+        }
+        else if (done)
+        {
             active = switchPlayer(active);
         }
         gameover();
@@ -132,13 +145,9 @@ public class Model {
     /**
      *
      **/
-    private int next(int pit) {
-        if (pit > LENGTH) {
-            pit = 0;
-        } else {
-            pit++;
-        }
-        return pit;
+    private void Done() {
+        done = true;
+        
     }
 
     /**
@@ -155,30 +164,42 @@ public class Model {
     }
 
     /**
-     * 
+     * saves the current board so it can be used by undo
      */
     public void saveBoard() {
-        boardSave = board.clone();
-
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < LENGTH - 1; j++)
+            {
+                boardSave[i][j] = board[i][j];
+            }
+        }
+            
     }
 
     /**/
-    public void undo() {
-        if (active == 0 && undoA > 0) {
-            board = boardSave.clone();
-            undoA--;
-        } else if (active == 1 && undoB > 0) {
-            board = boardSave.clone();
-            undoB--;
-        }
-        this.notifyListeners();
+    public void undo()
+    {
+        if(undos > 0);
+        {
+            for (int i = 0; i < 2; i++) 
+            {
+                for (int j = 0; j < LENGTH; j++)
+                {
+                    board[i][j] = boardSave[i][j];
+                }
+            }
+            undos--;
+        }            
+            this.notifyListeners();
+
     }
 
     /**
      * 
      * @return 
      */
-    public int[][] getBoard() {
+    public int[][] getBoard()
+    {
         return board;
     }
 
