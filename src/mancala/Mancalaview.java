1b6c6e1f231aa0a522f4bb40985ce0274d6ce0f9
Alex Souza <osouza21@gmail.com>
2012-12-01 21:42:56 -0800

    Added View Class

---
+++ src/mancala/Mancalaview.java
@@ -0,0 +1,110 @@
+
+package mancala;
+
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ *
+ * @author kai dao
+ */
+public class MancalaView implements ChangeListener{
+    
+    private static final int LENGTH = 7;
+    private int [][] board;
+    private JButton [][] pits;
+    private JButton undo;
+    private Model gameModel;
+    private JFrame frame; 
+    
+    public MancalaView(int startingPits){
+        gameModel = new Model(startingPits);
+        pits = new JButton[2][LENGTH];
+        board = gameModel.getBoard();
+        
+        frame = new JFrame();
+        frame.setLayout(new BorderLayout());
+        boardsetup();
+        
+        frame.add(pits[0][6], BorderLayout.WEST);
+        frame.add(pits[1][6], BorderLayout.EAST);
+        
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new GridLayout(0,6));        
+        topPanel.add(pits[0][5]);
+        topPanel.add(pits[0][4]);
+        topPanel.add(pits[0][3]);
+        topPanel.add(pits[0][2]);
+        topPanel.add(pits[0][1]);
+        topPanel.add(pits[0][0]);
+        frame.add(topPanel, BorderLayout.NORTH);
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new GridLayout(0,6));
+        bottomPanel.add(pits[1][0]);
+        bottomPanel.add(pits[1][1]);
+        bottomPanel.add(pits[1][2]);
+        bottomPanel.add(pits[1][3]);
+        bottomPanel.add(pits[1][4]);
+        bottomPanel.add(pits[1][5]);
+        frame.add(bottomPanel, BorderLayout.SOUTH);
+       
+        undo = new JButton("Undo player " + gameModel.getActive());
+        undo.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                gameModel.undo();
+                gameModel.notifyListeners();
+                
+                
+            }
+        });
+        frame.add(undo,BorderLayout.CENTER);
+        updatePits();
+        
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    private void boardsetup(){
+        for(int i = 0; i <= 1; i++){
+            for( int j = 0 ; j < LENGTH; j++){
+                pits[i][j] = new JButton();
+                final int pit = j;
+                final int side = i;
+                pits[i][j].addActionListener(new ActionListener(){
+                    
+                    public void actionPerformed(ActionEvent e) {
+                        gameModel.turn(pit, side);
+                        gameModel.notifyListeners();
+                    }
+                });
+            }
+        }
+        gameModel.attatch(this);
+    }
+    
+    private void updatePits(){
+        for(int i = 0; i < 2; i++ ){
+            for (int j = 0; j < LENGTH; j++){
+                pits[i][j].setText(board[i][j] + "");
+            }
+        }
+        undo.setText("Undo player " + gameModel.getActive());
+        frame.repaint();
+    }
+    
+    public void stateChanged(ChangeEvent e) {
+        board = gameModel.getBoard();
+        updatePits();
+        frame.repaint();
+    }
+}
